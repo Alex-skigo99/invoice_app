f4173ab6f49903167d32ab15568a2aad63564542
Alexander BOLSHUKHIN <skigo99@gmail.com>
2024-10-14 21:02:42 +0300

    add errorHandler middleware

---
+++ src/middleware/asyncHandler.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+
+// A function that takes another function and catches async errors
+export const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
