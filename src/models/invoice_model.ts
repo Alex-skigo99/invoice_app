f731e05fca693aec88d5954301c7a7dc258bb4da
Alexander BOLSHUKHIN <skigo99@gmail.com>
2024-10-17 18:55:10 +0300

    add swagger

--- src/models/invoice_model.ts
+++ src/models/invoice_model.ts
@@ -10,19 +10,19 @@ export const invoiceModel = {
         return await db.collection(collectionName).find(query).toArray()
     },
 
-    async create(invoice: Invoice) {
+    async create(invoice: Partial<Invoice>) {
         const result = await db.collection(collectionName).insertOne(invoice);
         return { ...invoice, _id: result.insertedId };
     },
 
-    async update(invoice: Invoice) {
-        await db.collection(collectionName).updateOne(
-            { _id: invoice._id },
+    async update(dbId: string, invoice: Partial<Invoice>) {
+        return await db.collection(collectionName).updateOne(
+            { '_id': new ObjectId(dbId) },
             { $set: { ...invoice } }
         );
     },
     async delete(dbId: string) {
-        await db.collection(collectionName).deleteOne({ _id: dbId as unknown as ObjectId });
+        return await db.collection(collectionName).deleteOne({ '_id': new ObjectId(dbId) });
     },
     async check() {
         await db.command({ ping: 1 });
